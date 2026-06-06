2d5bbdd7055b73de8372b2bdc2671f1e3c2a23b4
lhdht <15229684090@163.com>
2026-06-06 14:24:05 +0800

    fix: domain

--- agent/controller_test.go
+++ agent/controller_test.go
@@ -58,7 +58,7 @@ func testServiceNode(name string, port uint) *ServiceNode {
 		},
 		DNS:           name + ".default.svc.cluster.local",
 		Methods:       []string{fullMethod},
-		DescriptorRef: "https://minio.lhdht.cn/descriptor/" + name + "/v0.0.1.pb",
+		DescriptorRef: "https://minio.exmple.com/descriptor/" + name + "/v0.0.1.pb",
 		HTTPRoutes: []HTTPRoute{
 			{
 				HTTPMethod: "GET",
@@ -72,7 +72,7 @@ func testServiceNode(name string, port uint) *ServiceNode {
 func testGatewayManifest() *GatewayManifest {
 	return &GatewayManifest{
 		Schema:        GatewayManifestSchema,
-		DescriptorRef: "https://minio.lhdht.cn/descriptor/auth/v0.0.1.pb",
+		DescriptorRef: "https://minio.exmple.comom/descriptor/auth/v0.0.1.pb",
 		Services: []GatewayManifestService{
 			{
 				Name: "acme.auth.v1.AuthService",
